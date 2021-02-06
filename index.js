14393dd111bada63269bb44a513866c793c79375
bartolomej <bartolomej.kozorog@higroup.si>
2021-02-06 15:35:27 +0100

    add logs

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer(async (req, res) => {
   res.setHeader("Content-Type", "application/json");
+  console.log(`${req.method} ${req.url}`);
   try {
     const apiRes = await axios({
       method: 'get',
@@ -16,6 +17,7 @@ const server = http.createServer(async (req, res) => {
     res.statusCode = 200;
     res.end(JSON.stringify(apiRes.data));
   } catch (e) {
+    console.error(e);
     res.statusCode = 500;
     res.end(JSON.stringify({
       status: 'error',
